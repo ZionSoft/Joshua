5ecf18d959f81d34477758dbde046e1f3be9cb74
Xizhi Zhu <xizhi.zhu@gmail.com>
2017-07-07 22:21:38 +0300

    Added the missing drawer toggle.

--- app/src/main/java/net/zionsoft/joshua/reading/ReadingActivity.java
+++ app/src/main/java/net/zionsoft/joshua/reading/ReadingActivity.java
@@ -20,8 +20,11 @@ package net.zionsoft.joshua.reading;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 
 import net.zionsoft.joshua.R;
@@ -38,6 +41,9 @@ public final class ReadingActivity extends BaseActivity implements ReadingView {
         return new Intent(context, ReadingActivity.class);
     }
 
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+
     @BindView(R.id.toolbar)
     Toolbar toolbar;
 
@@ -50,13 +56,26 @@ public final class ReadingActivity extends BaseActivity implements ReadingView {
     @Inject
     ChapterPresenter chapterPresenter;
 
+    private ActionBarDrawerToggle drawerToggle;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_reading);
+
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, 0, 0);
+        drawerLayout.addDrawerListener(drawerToggle);
+
         chapters.setPresenter(chapterPresenter);
     }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
 
     @Override
     protected void onStart() {
@@ -70,5 +89,11 @@ public final class ReadingActivity extends BaseActivity implements ReadingView {
         presenter.dropView();
         chapters.onStop();
         super.onStop();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
     }
 }
